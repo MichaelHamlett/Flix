0a5501a1e9fb9d527797d2f653fb3e17ac046e8f
Michael Hamlett <michaelhamlett@fb.com>
2017-06-23 14:50:56 -0700

    Implemented collectionView and tab bar.

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Michael Hamlett on 6/22/17.
+//  Copyright © 2017 Michael Hamlett. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
