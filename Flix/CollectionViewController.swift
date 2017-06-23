327199293a9f8b92516180158e06f42866197c19
Michael Hamlett <michaelhamlett@fb.com>
2017-06-23 15:43:44 -0700

    Updated README.md and implemented minor UI Changes

--- Flix/CollectionViewController.swift
+++ Flix/CollectionViewController.swift
@@ -21,9 +21,23 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource {
         super.viewDidLoad()
         
         collectionView.dataSource = self
+        
+        
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        layout.itemSize = CGSize(width: width, height: width * 3/2)
+        
+        
+        
+        
         fetchMovies()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     
@@ -48,7 +62,7 @@ class CollectionViewController: UIViewController, UICollectionViewDataSource {
     
     func fetchMovies() {
         self.pleaseWait()
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a06d91c1c64b7b127defb0bec66fbfe4")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a06d91c1c64b7b127defb0bec66fbfe4&language=en-US&page=1")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
